e5633469ae5e62a644fc1ce26312d8497a0ef035
Vibhor Joshi <vibhorjoshi05@gmail.com>
2016-09-15 12:53:43 +0530

    ll delete

--- DSA/Linked List/LinkedListDelete.java
+++ DSA/Linked List/LinkedListDelete.java
@@ -8,26 +8,26 @@ public class LinkedListDelete {
 
 		LinkedList list = new LinkedList();
 		for (int i = 0; i < 10; i++) {
-			list.add((int)(Math.random()*100));
+			list.add((int) (Math.random() * 100));
 		}
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		LinkedListPrint.printList(list.getHead());
-		if(Integer.parseInt(br.readLine())==0)
-		while (true) {
-			int p = Integer.parseInt(br.readLine().trim());
-			if(p==-1)
-				break;
-			list.delete(p);
-			LinkedListPrint.printList(list.getHead());
-		}
-		else
-		{while (true) {
-			int p = Integer.parseInt(br.readLine().trim());
-			if(p==-1)
-				break;
-			list.deleteVal(p);
-			LinkedListPrint.printList(list.getHead());
-		}
+		if (Integer.parseInt(br.readLine()) == 0)
+			while (true) {
+				int p = Integer.parseInt(br.readLine().trim());
+				if (p == -1)
+					break;
+				list.delete(p);
+				LinkedListPrint.printList(list.getHead());
+			}
+		else {
+			while (true) {
+				int p = Integer.parseInt(br.readLine().trim());
+				if (p == -1)
+					break;
+				list.deleteVal(p);
+				LinkedListPrint.printList(list.getHead());
+			}
 		}
 	}
 
